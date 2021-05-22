1e38c3d99f00a3b4a46a0391a794574f0c6eae94
Gyeongmin Go <gkm2164@gmail.com>
2021-05-23 08:13:43 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {questions} from "./data/questions";
 import {
+  Box,
   Grid,
   Paper,
   Radio,
-  Select,
   Table,
   TableBody,
   TableCell,
@@ -29,26 +29,6 @@ interface QuestionProp {
   handleChange: (a: string, b: number, c: Answer) => void;
 }
 
-function Question({type, id, question, selectedValues, handleChange}: QuestionProp) {
-  return (
-    <Grid container>
-      <Grid item xs={7}>
-        {question}
-      </Grid>
-      <Grid item xs={3}>
-        {(['DD', 'SD', 'SA', 'DA'] as Answer[]).map(answer => <>
-          {answer}
-          <Radio
-            checked={selectedValues[type][id - 1] === answer}
-            onChange={() => handleChange(type, id, answer)}
-            value={answer}
-            name={`question-button-${type}-${id}`}
-            inputProps={{'aria-label': answer}}/></>)}
-      </Grid>
-    </Grid>
-  )
-}
-
 function App() {
   const [calculatedResult, setCalculatedResult] = useState<any[]>([]);
   const params = new URLSearchParams(window.location.search);
@@ -61,11 +41,11 @@ function App() {
   const [selectedValues, setSelectedValues] = useState<SelectedValues>(getOrDefault(
     params.get("data"),
     {
-    "external": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
-    "internal": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
-    "deeper": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
-    "spiritual": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD']
-  }));
+      "external": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
+      "internal": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
+      "deeper": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD'],
+      "spiritual": ['DD', 'DD', 'DA', 'DA', 'DD', 'DA', 'DD', 'DA', 'DA', 'DD']
+    }));
   const [externalScore, setExternalScore] = useState(0);
   const [internalScore, setInternalScore] = useState(0);
   const [deeperScore, setDeeperScore] = useState(0);
@@ -141,40 +121,41 @@ function App() {
   return (
     <div className="App">
       <Grid container>
-        <Grid item xs={4} />
+        <Grid item xs={4}/>
         <Grid item xs={4}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>척도</TableCell>
-                <TableCell>의미</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>DD</TableCell>
-                <TableCell>전혀 아니다</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>SD</TableCell>
-                <TableCell>안그런 편이다</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>SA</TableCell>
-                <TableCell>그런 편이다</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>DA</TableCell>
-                <TableCell>확실히 그렇다</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>척도</TableCell>
+                  <TableCell>의미</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>DD</TableCell>
+                  <TableCell>전혀 아니다</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>SD</TableCell>
+                  <TableCell>안그런 편이다</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>SA</TableCell>
+                  <TableCell>그런 편이다</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>DA</TableCell>
+                  <TableCell>확실히 그렇다</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
-        <Grid item xs={4} />
+        <Grid item xs={4}/>
       </Grid>
       <h1>외적인 삶: {externalScore}</h1>
+      <Indicator/>
       {questions.external.map(({id, question}) =>
         <Question type={"external"}
                   id={id}
@@ -182,6 +163,7 @@ function App() {
                   selectedValues={selectedValues}
                   handleChange={handleChange}/>)}
       <h1>내적인 삶: {internalScore}</h1>
+      <Indicator/>
       {questions.internal.map(({id, question}) =>
         <Question type={"internal"}
                   id={id}
@@ -189,6 +171,7 @@ function App() {
                   selectedValues={selectedValues}
                   handleChange={handleChange}/>)}
       <h1>심층적 삶: {deeperScore}</h1>
+      <Indicator/>
       {questions.deeper.map(({id, question}) =>
         <Question type={"deeper"}
                   id={id}
@@ -196,6 +179,7 @@ function App() {
                   selectedValues={selectedValues}
                   handleChange={handleChange}/>)}
       <h1>영적인 삶: {spiritualScore}</h1>
+      <Indicator/>
       {questions.spiritual.map(({id, question}) =>
         <Question type={"spiritual"}
                   id={id}
@@ -209,6 +193,41 @@ function App() {
       </Grid>
     </div>
   );
+}
+
+const Indicator = () => <Grid container>
+  <Grid item xs={6}/>
+  <Grid item xs={1}/>
+  <Grid item xs={1}>DD</Grid>
+  <Grid item xs={1}>SD</Grid>
+  <Grid item xs={1}>SA</Grid>
+  <Grid item xs={1}>DA</Grid>
+  <Grid item xs={1}/>
+</Grid>
+
+
+function Question({type, id, question, selectedValues, handleChange}: QuestionProp) {
+  return (<div>
+      <Grid container>
+        <Grid item xs={6}>
+          <Box alignItems="flex-start">
+            {question}
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box alignItems="flex-start">
+            {(['DD', 'SD', 'SA', 'DA'] as Answer[]).map(answer => <>
+              <Radio
+                checked={selectedValues[type][id - 1] === answer}
+                onChange={() => handleChange(type, id, answer)}
+                value={answer}
+                name={`question-button-${type}-${id}`}
+                inputProps={{'aria-label': answer}}/></>)}
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default App;
